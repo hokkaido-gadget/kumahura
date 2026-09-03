4f1c8f36c029a78fea972e6a28c0bf426b56765f
hokkaido-gadget <snogch2@gmail.com>
2026-09-03 14:29:17 +0900

    Update menu copy and skillet video start

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const updateHeader = () => {
 
 updateHeader();
 window.addEventListener("scroll", updateHeader, { passive: true });
+
+const hotVideo = document.querySelector(".hot-video[data-start]");
+
+if (hotVideo) {
+  const startAt = Number(hotVideo.dataset.start);
+  const seekToStart = () => {
+    if (Number.isFinite(startAt) && hotVideo.duration > startAt) {
+      hotVideo.currentTime = startAt;
+    }
+  };
+
+  if (hotVideo.readyState >= 1) {
+    seekToStart();
+  } else {
+    hotVideo.addEventListener("loadedmetadata", seekToStart, { once: true });
+  }
+
+  hotVideo.addEventListener("ended", () => {
+    seekToStart();
+    hotVideo.play().catch(() => {});
+  });
+}
